880e4e9c9f245d557dd7bfb47bcaf642454600e9
jeronimofjr <franciscojeronimo@alu.ufc.br>
2024-01-04 15:07:38 -0300

    add 3483 - Segundo Maior e Menor

---
+++ 3483.sql
@@ -0,0 +1,13 @@
+  (SELECT city_name,
+          population
+   FROM cities
+   ORDER BY population DESC
+   OFFSET 1
+   LIMIT 1)
+UNION ALL
+  (SELECT city_name,
+          population
+   FROM cities
+   ORDER BY population ASC
+   OFFSET 1
+   LIMIT 1);
